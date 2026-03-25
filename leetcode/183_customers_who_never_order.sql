07e69fc3bdefab4eae0095fda92a145ae0ac2ee0
Abhishek Kaushik <akaushik079@gmail.com>
2026-03-25 23:36:26 +0530

    LC-183: add solution using NOT IN

--- leetcode/183_customers_who_never_order.sql
+++ leetcode/183_customers_who_never_order.sql
@@ -1,5 +1,11 @@
+-- solution using JOIN and filter
 SELECT c.name AS Customers
 FROM Customers c
 LEFT JOIN Orders o
 ON c.id = o.customerId
 WHERE o.customerId IS NULL;
+
+-- solution using NOT IN
+SELECT name AS Customers
+FROM Customers
+WHERE id NOT IN (SELECT customerId FROM Orders);
